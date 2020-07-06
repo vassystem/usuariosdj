db7151c8fa81d5be5d5b50ce2eb63d98e8de7b9f
Ernesto <netiovq@gmail.com>
2020-07-06 16:47:43 -0600

    creacion de modelos

--- usuarios/applications/users/models.py
+++ usuarios/applications/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 # Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    GENDER_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'FEMENINO'),
+    )
+
+
+    username = models.CharField(max_length=10, unique=True)
+    email = models.EmailField()
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    genero = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    USERNAME_FIELD = 'username'
+
+    def get_short_name(self):
+        return self.username
+
+    def get_full_name(self):
+        return self.nombres + ' ' + self.apellidos
