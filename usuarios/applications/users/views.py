4badce1eb55efadd0f8dff531919e81661dbc3f0
Ernesto <netiovq@gmail.com>
2020-07-06 17:29:09 -0600

    nuevos archivos 1

--- usuarios/applications/users/views.py
+++ usuarios/applications/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import (
+    CreateView
+)
 # Create your views here.
+
+
+class UserRegisterView(CreateView):
+    template_name = "users/register.html"
