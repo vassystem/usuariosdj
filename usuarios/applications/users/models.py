a37c717633837b7b2f632e3becd7646556ba45dc
Ernesto <netiovq@gmail.com>
2020-07-06 17:14:54 -0600

    creacion de modelos

--- usuarios/applications/users/models.py
+++ usuarios/applications/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 # Create your models here.
-
+from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
 
@@ -13,11 +13,17 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     username = models.CharField(max_length=10, unique=True)
     email = models.EmailField()
-    nombres = models.CharField(max_length=30)
-    apellidos = models.CharField(max_length=30)
+    nombres = models.CharField(max_length=30,  blank=True)
+    apellidos = models.CharField(max_length=30,  blank=True)
     genero = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    #
+    is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
+
+    REQUIRED_FIELDS = ['email',]
+
+    objects = UserManager()
 
     def get_short_name(self):
         return self.username
